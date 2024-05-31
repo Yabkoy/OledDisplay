552b300a4ce6d5ecf40ac37bb9c6bd2b5a9d1ea7
Yabkoy <yabuszko05@gmail.com>
2024-06-01 00:20:12 +0200

    Fix displayBuffer init function, that didn't setup a width and height for displayBuffer object

--- src/displayBuffer.c
+++ src/displayBuffer.c
@@ -1,6 +1,9 @@
 #include "displayBuffer.h"
 
 void initDisplayBuffer(displayBuffer* dBuffer, size_t width, size_t height){
+    dBuffer->width = width;
+    dBuffer->height = height;
+
     dBuffer->bufferLen = (width*height)*sizeof(uint8_t);
     dBuffer->buffer = (uint8_t*)malloc(dBuffer->bufferLen);
     memset(dBuffer->buffer, 0, dBuffer->bufferLen);
