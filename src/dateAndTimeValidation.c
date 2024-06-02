fdf7eaee25449f3b221d9524e6d4dd6d0374a3c6
Yabkoy <yabuszko05@gmail.com>
2024-06-03 01:01:53 +0200

    Add day of week calculation algorithm, and ds3231_datetime setup function

--- src/dateAndTimeValidation.c
+++ src/dateAndTimeValidation.c
@@ -31,6 +31,20 @@ void validateDayOfMonth(uint8_t* day, uint8_t month, uint16_t year){
 	validateTimeUnits(day, 1, 30+isMonthHave31Days(month));
 }
 
+//Algorithm from: "https://www.geeksforgeeks.org/find-day-of-the-week-for-a-given-date/"
+int getDayOfWeek(int d, int m, int y)
+{
+    static int t[] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+    y -= m < 3;
+    return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d)
+           % 7;
+}
+void setCorrectDayOfWeek(ds3231_datetime_t* dateTime){
+	printf("DAY: %d, MON: %d, YEAR: %d\n", dateTime->day, dateTime->month, dateTime->year);
+	uint8_t dowIndex = getDayOfWeek(dateTime->day, dateTime->month, dateTime->year);
+	dowIndex = (dowIndex == 0)? 6 : dowIndex-1;
+	dateTime->dotw = dowIndex;
+}
 
 void validateDateTime(ds3231_datetime_t* dateTime){
 	validateTimeUnits(&dateTime->hour, 0, 23);
--- src/dateAndTimeValidation.h
+++ src/dateAndTimeValidation.h
@@ -9,6 +9,8 @@ void validateTimeUnits(uint8_t* timeUnit, uint8_t min, uint8_t max);
 
 void validateYear(uint16_t* year, uint16_t min, uint16_t max);
 void validateDayOfMonth(uint8_t* day, uint8_t month, uint16_t year);
+int getDayOfWeek(int d, int m, int y);
+void setCorrectDayOfWeek(ds3231_datetime_t* dateTime);
 void validateDateTime(ds3231_datetime_t* dateTime);
 
 #endif
